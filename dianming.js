b4cfc9d7896018685462c9250ad14bd888769b0a
aoba98 <nadph513@outlook.com>
2021-11-07 20:58:31 +0800

    fix: page.goto() Timeout

--- dianming.js
+++ dianming.js
@@ -14,40 +14,38 @@ async function report() {
     const context = browser.defaultBrowserContext();
     await context.overridePermissions('https://jinshuju.net', ['geolocation']);
 
-    await page.goto('https://jinshuju.net/f/9wGVFn/', {
-        waitUntil:"networkidle0"
-    });
+    await page.goto('https://jinshuju.net/f/kDiOo6', {waitUntil: "domcontentloaded"});
     try{
-        await page.type('input.field_6', config.name);
+        await page.waitForSelector(".field_1 input");
+        await page.type('.field_1 input', config.name);
         console.log('fillin name');
-        await page.type('input.field_5', config.id);
+        await page.type('input.field_2', config.id);
         console.log('fillin id');
-        await page.click('input[value="Povx"]');
+        await page.click('input[value="Ir9F"]');
+        await page.click('input[value="KsBW"]');
+        await page.click('input[value="srmW"]');
         console.log('check 1');
-        await page.waitForSelector('div.field_24 input[value="FRjZ"]');
-        await page.click('div.field_24 input[value="FRjZ"]');
+        await page.waitForSelector('div[data-api-code="field_6"] input[value="JsBh"]');
+        await page.click('div[data-api-code="field_6"] input[value="JsBh"]');
         console.log('check 2');
-        await page.waitForSelector('div.field_7 input[value="FRjZ"]');
-        await page.click('div.field_7 input[value="FRjZ"]');
+        await page.waitForSelector('div[data-api-code="field_4"] input[value="zBCK"]');
+        await page.click('div[data-api-code="field_4"] input[value="zBCK"]');
         console.log('check 3');
-        await page.waitForSelector('div.field_9 input[value="Zda4"]');
-        await page.click('div.field_9 input[value="Zda4"]');
+        await page.waitForSelector('div[data-api-code="field_7"] input[value="GaXt"]');
+        await page.click('div[data-api-code="field_7"] input[value="GaXt"]');
         console.log('check 4');
-        await page.setGeolocation({ latitude: config.lat, longitude:  config.lon});
-        await page.waitForSelector('div.field_21 button');
-        await page.click('div.field_21 button');
+        await page.setGeolocation({ latitude: 30.75233, longitude: 103.92567 });
+        await page.waitForSelector('div.field_16 button');
+        await page.click('div.field_16 button');
         console.log('get pos');
     
         await page.waitForSelector('div.geo-filed__header-latlng span');
-        await page.click('div.published-form__footer-buttons');
-        console.log('submmit');
+        // await page.evaluate(()=>{window.alert('submmit')});
+        await page.click('div.published-form__footer-buttons button');
+        // console.log('submmit');
 
         await page.waitForSelector('div.message');
-        if (config.save_screenshot) {
-            await page.screenshot( config.dm_screenshot_dir + new Date().toLocaleDateString() + '.png');
-            console.log("📂已生成截图");
-        }
-        console.log('check success');
+        // console.log('check success');
     }
     catch(e){
         console.log("something wrong");
