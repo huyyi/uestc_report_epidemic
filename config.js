5bc389e276503daf33a3591e336dc26dd2e9949a
aoba98 <nadph513@outlook.com>
2021-11-07 22:06:24 +0800

    迁移到puppeteer-core

--- config.js
+++ config.js
@@ -1,21 +1,18 @@
 const config = {
     // 环境与配置
-    chrome_exepath: "chrome\\chrome78-win\\chrome.exe", // chrome路径
+    chrome_exepath: "Chrome\\chrome.exe", // chrome路径
     headless:false,
     save_screenshot: false, // 是否保存填报成功截图
 
     // 每日打卡信息
     user_id: "", // 学号
     passwd: "", // 统一认证密码
-    screenshot_dir: "",
-    
 
     // 晚点名信息
     name:"",
     id:"",
     lat: 30, // 模拟定位经纬度
     lon: 106,
-    dm_screenshot_dir: ""
 }
 
 module.exports = config
